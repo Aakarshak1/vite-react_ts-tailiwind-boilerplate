c98387218c29a0aee8f4ef59cf1e1fff644eb776
Aakarshak <akku_sthapak@live.com>
2024-04-22 23:21:26 +0530

    Lint fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.less';
 
-const root = document.getElementById('root') as HTMLElement;
+const root = document.getElementById('root')!;
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
