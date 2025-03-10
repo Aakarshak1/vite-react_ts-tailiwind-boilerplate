e3b48f039f48f44ae0dc75d5ce409ff0ee49a468
Aakarshak <akku_sthapak@live.com>
2025-03-11 01:10:49 +0530

    chore: :zap: updated vite and vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  server: { 
+    watch: { 
+      ignored: ['node_modules'] 
+    } 
+  },
   test: {
     globals: true,
     environment: 'jsdom',
