40a121e201024a49802d1b8a0814f563aa5bd870
Aakarshak Sthapak <akku_sthapak@live.com>
2025-03-12 00:51:46 +0530

    chore: :zap: tailwindcss v4

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vite-react_ts-tailiwind-boilerplate
+
+- TODO
+    - Upgrade `eslint-plugin-tailwindcss` when support for v4 drop
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     tsconfigPaths(),
     eslint({
